67f55aaba35f2d809e7d8b2e3af8d17a47b14a6f
4n7hem <48725919+4n7hem@users.noreply.github.com>
2021-10-25 15:20:14 -0300

    Funções básicas do jogo supostamente terminadas

--- Row.java
+++ Row.java
@@ -1,5 +1,3 @@
-package stopjava;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,21 +9,39 @@ public class Row {
         this.nomeDaCategoria = categoria;
         this.jogadorResposta = new HashMap<String,String>();
     }
-
+    
     public void inserirResposta(String jogador, String resposta){
         jogadorResposta.put(jogador, resposta);
     }
-
-    public Map<String, Integer> calcularFrequencia(){
-        Map<String, Integer> resultado = new HashMap<String, Integer>();
-        /*Esse método precisa contar a frequência de palavras repetidas no jogadorResposta */
-        
-            for (String seq : jogadorResposta.values()) {
-                Integer count = resultado.get(seq);
-                resultado.put(seq, count == null ? 1 : count + 1);
+    
+    public int contarRepeticao(String palavra){
+        int count = 0;
+        for(Map.Entry<String, String> entry : jogadorResposta.entrySet()){
+            //o lowercase é para filtrar palavras iguais mas com caracteres diferentes
+            if(entry.getValue().equals(palavra.toLowerCase())){
+                count++;
             }
+        }
+        return count;
+    }
 
+    public Map<String, Integer> calcularFrequencia(){ //conta a quantidade de repeticoes de respostas
+        Map<String, Integer> resultado = new HashMap<String, Integer>();
+        
+        for(Map.Entry<String, String> entry : jogadorResposta.entrySet()){
+            String palavra = entry.getValue();
+            String jogador = entry.getKey();
+            int frequencia = contarRepeticao(palavra);
+            //registra o user e quantas vezes a resposta dele foi usada na categoria
+            resultado.put(jogador, frequencia); 
+        }
+        //o retorno é passado de volta ao Jogo.
         return resultado;
     }    
     
 }
+
+
+
+
+
