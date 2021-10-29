341c33b3fcd29555d8f96c1be4ec57f096482647
4n7hem <48725919+4n7hem@users.noreply.github.com>
2021-10-29 20:24:31 -0300

    Agora a pontuação tá certinho

--- Row.java
+++ Row.java
@@ -15,10 +15,29 @@ public class Row {
         jogadorResposta.put(jogador, resposta);
     }
 
-    public Map<String, Integer> calcularFrequencia(){
-        Map<String, Integer> resultado = new HashMap<String, Integer>();
-        /*Esse método precisa contar a frequência de palavras repetidas no jogadorResposta */
-        return resultado;
+    public int contarRepeticao(String palavra){
+        int count = 0;
+        for(Map.Entry<String, String> entry : jogadorResposta.entrySet()){
+            //o lowercase é para filtrar palavras iguais mas com caracteres diferentes
+            if(entry.getValue().equals(palavra.toLowerCase())){
+                count++;
+            }
+        }
+        return count;
     }
 
+    public Map<String, Integer> calcularFrequencia(){ //conta a quantidade de repeticoes de respostas
+        Map<String, Integer> resultado = new HashMap<String, Integer>();
+        
+        for(Map.Entry<String, String> entry : jogadorResposta.entrySet()){
+            String palavra = entry.getValue();
+            String jogador = entry.getKey();
+            int frequencia = contarRepeticao(palavra);
+            //registra o user e quantas vezes a resposta dele foi usada na categoria
+            resultado.put(jogador, frequencia); 
+        }
+        //o retorno é passado de volta ao Jogo.
+        return resultado;
+    }    
+    
 }
